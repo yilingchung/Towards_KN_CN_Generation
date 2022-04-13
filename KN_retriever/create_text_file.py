54cf5b53704739cb9e19c3790ec65b5719b1b33d
Yi-Ling Chung <yilingchung27@gmail.com>
2022-04-13 16:30:31 +0200

    added script for creating text file

---
+++ KN_retriever/create_text_file.py
@@ -0,0 +1,13 @@
+import pandas
+
+"""This script prepares files to run keyphrase digger over.
+It creates a txt file for each hate speech and counter narrative in CONAN."""
+
+def create_txt(output_directory, text, id):
+    with open(output_directory+str(id)+".txt", "w") as f:
+        f.write(text.strip())
+
+if __name__ == '__main__':
+    df = pandas.read_csv("data/CONAN.csv")
+    df.apply(lambda x: create_txt("data/HS/", x['hateSpeech'], x['cn_id']), axis=1)
+    df.apply(lambda x: create_txt("data/CN/", x['counterSpeech'], x['cn_id']), axis=1)
