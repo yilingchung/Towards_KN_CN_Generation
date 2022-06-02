b028c8e0d8b12824c32a34d8adffdb99c4ba3ac8
Yi-Ling Chung <ychung@fbk.eu>
2022-06-02 22:18:12 +0200

    Update extract_keyphrase.py

--- KN_retriever/extract_keyphrase.py
+++ KN_retriever/extract_keyphrase.py
@@ -4,6 +4,7 @@ import pandas
 
 def extract_keyphrase(directory, cn_id):
     df_kp = pandas.read_csv(directory+str(cn_id)+".tsv", sep="\t")
+    df_kp = df_kp.astype({"keyword": str})
     kp = df_kp["keyword"].tolist()
     num_kp = len(kp)     # calculate the number of keyphrases
     if num_kp > 0:
@@ -15,6 +16,6 @@ def extract_keyphrase(directory, cn_id):
 
 if __name__ == '__main__':
     df = pandas.read_csv("data/conan.csv")
-    df[['cn_keyword', 'num_cn_keyword']] = df.apply(lambda x: extract_keyphrase("data/CN/", x['cn_id']), axis=1)
-    df[['hs_keyword', 'num_hs_keyword']] = df.apply(lambda x: extract_keyphrase("data/HS/", x['cn_id']), axis=1)
+    df[['cn_keyword', 'num_cn_keyword']] = df.apply(lambda x: extract_kp("data/CN/", x['cn_id']), axis=1)
+    df[['hs_keyword', 'num_hs_keyword']] = df.apply(lambda x: extract_kp("data/HS/", x['cn_id']), axis=1)
     df.to_csv("data/conan_hscnkp.csv", index=False)
