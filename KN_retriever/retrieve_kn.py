8ddfab02e3830cfe38ef493e1ef80876d0e81c53
Yi-Ling Chung <ychung@fbk.eu>
2022-06-02 22:50:06 +0200

    Update retrieve_kn.py

--- KN_retriever/retrieve_kn.py
+++ KN_retriever/retrieve_kn.py
@@ -25,15 +25,11 @@ def form_query(queries, boolean):
     link = "http://localhost:8989/solr/knowledgecollection/select?q=" + query + "&rows=25&wt=json"
     return link
 
-def call_solr(hs_kp, cn_kp, gen_kp, boolean, kp_type):
+def call_solr(hs_kp, cn_kp, boolean, kp_type):
     if kp_type == 'hscn':
         keyphrase = hs_kp + cn_kp
-    elif kp_type == 'hsgen':
-        keyphrase = hs_kp + gen_kp
     elif kp_type == 'hs':      
         keyphrase = hs_kp
-    elif kp_type == 'gen':
-        keyphrase = cn_kp
     query = form_query(keyphrase, boolean)
     connection = urllib.request.urlopen(query)
     response = json.load(connection)
@@ -41,20 +37,22 @@ def call_solr(hs_kp, cn_kp, gen_kp, boolean, kp_type):
 
 def process_keyphrase(text):
     # clean special characters in keyphrases
-    return text.replace("#", "").replace("@", "").replace("!", "").replace("’", "'").replace("“", "").replace("”", "").replace("?","?")
+    t = text.replace("'", "").replace("#", "").replace("@", "").replace("!", "")
+    t = t.replace("’", "'").replace("“", "").replace("”", "").replace("?","?")
+    return t
 
-def main(num_hskp, num_cnkp, hs_kp, cn_kp, gen_kp, cn_id, output_dir, kp_type):
+def main(hs_kp, cn_kp, cn_id, output_dir, kp_type):
     print("Current id: ", cn_id)
     kn_output_path = f'{output_dir}/{str(id)}.json'
 
     # retrieve relevant document using keyphrases as queries with Solr
-    if str(num_hskp) != "0" and str(num_cnkp) != "0":
+    if len(hs_kp) > 0 and len(cn_kp) > 0:
         json_list = []
         hskp = process_keyphrase(hs_kp).split(", ")
         cnkp = process_keyphrase(cn_kp).split(", ")
-        genkp = process_keyphrase(gen_kp).split(", ")
+        genkp = ''
         boolean = "||"
-        response = call_solr(hskp, cnkp, genkp, boolean, kp_type)
+        response = call_solr(hskp, cnkp, boolean, kp_type)
         while response['response']['numFound'] == 0:
             if len(hskp) > 0 and len(cnkp) > 0:
                 if len(hskp) > len(cnkp):
@@ -69,7 +67,7 @@ def main(num_hskp, num_cnkp, hs_kp, cn_kp, gen_kp, cn_id, output_dir, kp_type):
                 hskp = process_keyphrase(hs_kp).split(", ")
                 cnkp = process_keyphrase(cn_kp).split(", ")
                 boolean = "||"
-            response = call_solr(hskp, cnkp, genkp, boolean, kp_type)
+            response = call_solr(hskp, cnkp, boolean, kp_type)
             
         # return retrieved document in json, one json file contains all the retrieved documents.
         # Each json is a list of dictionary containing "title", "text" and "summary" (if exists)
@@ -90,5 +88,8 @@ if __name__ == '__main__':
     args = parse_args()
     df = pandas.read_csv(args.input_filename)
     # df = df[df['split'] == args.data_split]
-    df.apply(lambda x: main(x['num_hs_keyword'], x['num_cn_keyword'], x['hs_keyword'], x['cn_keyword'], x['generated_cn_keywords'], x['cn_id'], args.output_dir, args.kp_type), axis=1)
+    # df.apply(lambda x: main(x['num_hs_keyword'], x['num_cn_keyword'], x['hs_keyword'], x['cn_keyword'], x['generated_cn_keywords'], x['cn_id'], args.output_dir, args.kp_type), axis=1)
+    df.apply(lambda x: main(x['hs_keyword'], x['cn_keyword'], x['cn_id'], args.output_dir, args.kp_type), axis=1)
     # df.to_csv(args.output_filename)
+    
+    
