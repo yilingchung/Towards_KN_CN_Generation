413c1e8f16eebf5848e45bb5af66b7ffb37259f5
Yi-Ling Chung <yilingchung27@gmail.com>
2024-05-05 15:45:09 +0100

    Update data path

--- KN_retriever/run_kd.sh
+++ KN_retriever/run_kd.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-for file in CN/*.txt; do
+for file in data/CN/*.txt; do
 	java -jar KD.jar -lang ENGLISH --no_idf -cp -p MEDIUM -l 1 -us -v -n 5 -m 6 $file
 done
 
-for file in HS/*.txt; do
+for file in data/HS/*.txt; do
 	java -jar KD.jar -lang ENGLISH --no_idf -cp -p MEDIUM -l 1 -us -v -n 5 -m 6 $file
 done
